28739eb4d6cb1adfb7c3004ad13cc71f87dc7c97
hm7md <56906490+hm7md@users.noreply.github.com>
2023-02-15 13:13:36 +0400

    Add files via upload

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,3 +1,61 @@
+
+var myInput = document.getElementById("password");
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
+
+// When the user clicks on the password field, show the message box
+myInput.onfocus = function() {
+  document.getElementById("message").style.display = "block";
+}
+
+// When the user clicks outside of the password field, hide the message box
+myInput.onblur = function() {
+  document.getElementById("message").style.display = "none";
+}
+
+// When the user starts to type something inside the password field
+myInput.onkeyup = function() {
+  // Validate lowercase letters
+  var lowerCaseLetters = /[a-z]/g;
+  if(myInput.value.match(lowerCaseLetters)) {
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+}
+
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z]/g;
+  if(myInput.value.match(upperCaseLetters)) {
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+  // Validate numbers
+  var numbers = /[0-9]/g;
+  if(myInput.value.match(numbers)) {
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+
+  // Validate length
+  if(myInput.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+}
 $(document).ready(function() { 
   
   $("#account-select").change(function() {
@@ -12,12 +70,12 @@ $(document).ready(function() {
   
   $("#yes").click(function(){
     $("#current-address").show('slide');
-    $("#non-uk").hide();
+    $("#non-uae").hide();
     window.scrollTo(0,document.body.scrollHeight);
   });
   
   $("#no").click(function(){
-    $("#non-uk").show('slide');
+    $("#non-uae").show('slide');
     $("#current-address").hide();
     window.scrollTo(0,document.body.scrollHeight);
   });
